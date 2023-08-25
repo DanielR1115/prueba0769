ff11c4afbbeaf84ed115f6c2737d3d8e636eb593
DanielR1115 <143163519+DanielR1115@users.noreply.github.com>
2023-08-25 00:53:10 -0600

    prueba

---
+++ 226_07.c
@@ -0,0 +1,15 @@
+#include <Stdio.h>
+#include <stdlib.h>
+/*
+Calcula el area de un circulo cuyo radio es de 8.9cm Tome pi como 3.14
+*/
+int main() {
+    double pi = 3.14;
+    double radio = 8.90;
+    double area = pi * radio;
+    system("cls");
+    printf("The area of the give circle is %lf \n", area ); //%d es para imprimir enteros(int) y %lf es para imprimir decimales(double)
+ 
+    return 0;
+}
+/*Since now we write any program in english*/
